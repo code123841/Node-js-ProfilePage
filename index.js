d80b4ff0bdd29a3046893a7642f9df107de8ac86
Udhay <57910021+code123841@users.noreply.github.com>
2021-12-01 07:19:54 +0530

    mainpage commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-
+const ejs = require('ejs');
 const port = 5000;
+const fs = require('fs')
+// setting up static files 
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/fonts' , express.static(__dirname + 'public/fonts'))
+app.use('/img' , express.static(__dirname + 'public/img'))
 
-app.get('/',(req,res) => {
-    res.send("This is a sample express app")
-})
+app.set('view engine' , 'ejs')
+
+
+app.get('/', (req, res) => {
+    // console.log("user has entered the home page")
+    res.render('index/index')
+  });
 
 // Listen on port 5000
 app.listen(port, () => {
